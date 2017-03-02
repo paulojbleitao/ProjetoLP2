c04f55545c213445df988fc025cbf4f182887425
Edvaldo <edvaldo.neto@ccc.ufcg.edu.br>
2017-03-02 12:02:18 -0300

    :thinking:

---
+++ src/Anotacoes.java
@@ -0,0 +1,26 @@
+
+public class Anotacoes {
+
+	/*
+	 * Pessoa:
+	 * - String cpf;
+	 * - String nome;
+	 * - String email;
+	 * # gets e sets pra tudo;
+	 * # pessoa sera identificada pelo cpf;
+	 * 
+	 * Projeto:
+	 * - String nome;
+	 * - String objetivo;
+	 * - String dataInicio;
+	 * - int duracao;
+	 * - String codigo;
+	 * - double custoTotal;
+	 * - double desConstante;
+	 * - double desEventual;
+	 * - double desCapital;
+	 * # gets e sets pra tudo;
+	 * # projeto sera identificado pelo codigo;
+	 * 
+	 */
+}
